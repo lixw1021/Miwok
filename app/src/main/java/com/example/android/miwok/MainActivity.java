d470fc104d98065ab86294d6a23077ceae1ec2b3
xianwei li <xil148@pitt.edu>
2017-08-07 17:19:31 -0400

    added adapter

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,10 +15,26 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.numbers)
+    TextView numbersView;
+    @BindView(R.id.family)
+    TextView familyView;
+    @BindView(R.id.colors)
+    TextView colorsView;
+    @BindView(R.id.phrases)
+    TextView phrasesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +42,33 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
+
+    @OnClick(R.id.numbers)
+    public void openNumbers(View view) {
+        Intent intent = new Intent(this, NumbersActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.family)
+    public void openFamily(View view) {
+        Intent intent = new Intent(this, FamilyActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.colors)
+    public void openColors(View view) {
+        Intent intent = new Intent(this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.phrases)
+    public void openPhrases(View view) {
+        Intent intent = new Intent(this, PhrasesAvtivity.class);
+        startActivity(intent);
+    }
+
+
 }
